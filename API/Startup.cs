27621c852d79926dd4ab0b8f31ff5dc8e80542e8
ARAleksiev <al.aleksiev@mail.bg>
2021-04-15 13:53:07 +0300

    react section

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(this.config.GetConnectionString("DefaultConnection")));
+            services.AddCors(opt=> opt.AddPolicy("CorePolicy", policy=>policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http//localhost:3000")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +51,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorePolicy");
 
             app.UseAuthorization();
 
